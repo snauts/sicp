e9fbcce4128acf538ee3d7829d3b67cee68a33da
arturs <arturs@chestnut>
2011-06-14 07:48:00 +0300

    fix 3.69 triples function not to emit duplicate i==j==k triples, however it does not change results of test-3.69 function that generates Pythagorean triples (dedicated to github.com/einars)

--- chapter3.scm
+++ chapter3.scm
@@ -2633,7 +2633,7 @@
   (cons-stream
    (list (stream-car s) (stream-car t) (stream-car u))
    (interleave
-    (stream-map (lambda (x) (cons (stream-car s) x)) (pairs t u))
+    (stream-map (lambda (x) (cons (stream-car s) x)) (stream-cdr (pairs t u)))
     (triples (stream-cdr s) (stream-cdr t) (stream-cdr u)))))
 
 (define pythagorean-triples
